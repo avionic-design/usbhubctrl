e2dc2fe6f855dcc4d2694abe694b318f415c7004
Meike Vocke <meike.vocke@avionic-design.de>
2016-11-14 17:31:14 +0100

    Sleep after writing the EEPROM
    Sleep for the maximum write cycle time of 5 ms for the 25LC640 EEPROM to
    guarantee that the EEPROM has been written successfully.
    
    Change-Id: I1f46923109aaa8071ccfcb0d1f576319838d562d
    Signed-off-by: Meike Vocke <meike.vocke@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/6918
    Reviewed-by: Bert van Hall <bert.vanhall@avionic-design.de>

--- src/usb_eeprom.c
+++ src/usb_eeprom.c
@@ -3,12 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <unistd.h>
+
 #include "usb_eeprom.h"
 
 int usb_eeprom_erase(usb_dev_handle *uh, size_t size)
 {
 	uint8_t *erase_buf;
-	int len;
+	int ret_val = 0;
+
 	if (!uh || !size)
 		return -EINVAL;
 	if (size > MAX_EEPROM_SIZE)
@@ -19,13 +22,11 @@ int usb_eeprom_erase(usb_dev_handle *uh, size_t size)
 		return -ENOMEM;
 	memset(erase_buf, 0xff, size);
 
-	len = usb_control_msg(uh, USB_REQ_TYPE_WRITE_EEPROM,
-			USB_REQ_WRITE, 0, 0, (char *)erase_buf, size,
-			CTRL_TIMEOUT);
+	ret_val = usb_eeprom_write(uh, erase_buf, size);
 
 	free(erase_buf);
 
-	return len;
+	return ret_val;
 }
 
 int usb_eeprom_read(usb_dev_handle *uh, uint8_t *buffer, size_t size)
@@ -54,6 +55,13 @@ int usb_eeprom_write(usb_dev_handle *uh, uint8_t *buffer, size_t size)
 
 	len = usb_control_msg(uh, USB_REQ_TYPE_WRITE_EEPROM, USB_REQ_WRITE,
 			0, 0, (char *)buffer, size, CTRL_TIMEOUT);
+
+	/*
+	 * Sleep for more than 5 ms to guarantee EEPROM data is written.
+	 * This is the maximal write cycle time of the EEPROM, specified
+	 * in the 25AA640/25LC640 datasheet.
+	 */
+	usleep(5500);
 
 	return len;
 }
